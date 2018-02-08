0d63533b7b49e76c7e32a2dbd7fa3ee6b435f3aa
jea74 <jea74@drexel.edu>
2018-02-08 17:13:51 -0500

    Push changes

---
+++ fakefile.c
@@ -0,0 +1,18 @@
+//Fakefile
+/*
+this is a test.
+I want to see this line in the text file.
+If i dont see this line, I will freak out
+
+stuff should be untouched inside this text file.
+wt2324t
+24f24
+r42
+42f
+
+thisthis file is also a test.
+Not sure what to write tbh.
+thing assignment is bs, but i think I'm learning.
+this this this this this this this.
+
+*/
