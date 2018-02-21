b80da569ccd5434602ccd93f69b61fd2d5da2ce8
jea74 <jea74@drexel.edu>
2018-02-20 21:45:46 -0500

    Test file descriptors

--- csapp.c
+++ csapp.c
@@ -452,7 +452,7 @@ struct hostent *Gethostbyaddr(const char *addr, int len, int type)
 /************************************************
  * Wrappers for Pthreads thread control functions
  ************************************************/
-
+/*
 void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp, 
 		    void * (*routine)(void *), void *argp) 
 {
@@ -475,16 +475,18 @@ void Pthread_join(pthread_t tid, void **thread_return) {
     if ((rc = pthread_join(tid, thread_return)) != 0)
 	posix_error(rc, "Pthread_join error");
 }
-
+*/
 /* $begin detach */
+/*
 void Pthread_detach(pthread_t tid) {
     int rc;
 
     if ((rc = pthread_detach(tid)) != 0)
 	posix_error(rc, "Pthread_detach error");
 }
+*/
 /* $end detach */
-
+/*
 void Pthread_exit(void *retval) {
     pthread_exit(retval);
 }
@@ -496,11 +498,11 @@ pthread_t Pthread_self(void) {
 void Pthread_once(pthread_once_t *once_control, void (*init_function)()) {
     pthread_once(once_control, init_function);
 }
-
+*/
 /*******************************
  * Wrappers for Posix semaphores
  *******************************/
-
+/*
 void Sem_init(sem_t *sem, int pshared, unsigned int value) 
 {
     if (sem_init(sem, pshared, value) < 0)
@@ -518,7 +520,7 @@ void V(sem_t *sem)
     if (sem_post(sem) < 0)
 	unix_error("V error");
 }
-
+*/
 /*********************************************************************
  * The Rio package - robust I/O functions
  **********************************************************************/
